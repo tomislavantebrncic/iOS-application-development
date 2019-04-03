1f85bebb496cccae3729013e3f80dee02b68ee5a
tomislavantebrncic <tomislav-ante.brncic@fer.hr>
2019-04-04 00:07:21 +0200

    Create project for hw01

---
+++ iOSVjestina2019/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSVjestina2019
+//
+//  Created by FIVE on 03/04/2019.
+//  Copyright © 2019 FIVE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
